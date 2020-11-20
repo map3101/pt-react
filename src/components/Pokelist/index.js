cf415dd18e3626722d3ddcf488a111f45adbde2d
map3101 <marcelo.postiglione@gmail.com>
2020-11-20 01:22:36 -0300

    Renderizando dinamicamente a cor do fundo de cada pokémon baseado em seu tipo

--- src/components/Pokelist/index.js
+++ src/components/Pokelist/index.js
@@ -3,9 +3,42 @@ import './style.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const types = {
+    bug: "#7ED578",
+    electric: "#FFF34B",
+    fairy: "#FF7EE5",
+    fighting: "#F17373",
+    fire: "#FFB433",
+    flying: "#D7F1E9",
+    ghost: "#E2E2E2",
+    grass: "#5eff53",
+    ground: "#AA8546",
+    ice: "#AEE3FB",
+    normal: "#D7DBA8",
+    poison: "#CE52F9",
+    psychic: "#FFC157",
+    rock: "#757575",
+    steel: "#A1A1A1",
+    water: "#7192FF",
+    dragon: "#43372D"
+}
+
+function getColor(type) {
+    var pos = type.search(";"); 
+    if (pos === -1) {
+        return 'linear-gradient(' + types[type] + "," + types[type] + "aa)"
+    }
+    else{
+        var type1 = type.slice(0,pos);
+        var type2 = type.slice(pos+1);
+        return 'linear-gradient(to right,' + types[type1] + " 50%," + types[type2] + " 50.001%)"
+    }
+}
+
 export default function Pokelist({ page }) {
     
     const [pokemons, setPokemons] = useState([]);
+
     
     useEffect(() => {
         axios.get(`https://pokedex20201.herokuapp.com/pokemons?page=${ page }`)
@@ -15,12 +48,13 @@ export default function Pokelist({ page }) {
 
     console.log(pokemons);
 
+
     return(
         <div className="pokemons">
-            <input placeholder="Buscar pokemon" />
+            <input placeholder="Buscar Pokémon" />
             <ul>
                 {pokemons.map((pokemon) => (
-                    <li className="pokemon" key={pokemon.id}>
+                    <li style={{background: getColor(pokemon.kind) }} className="pokemon" key={pokemon.id}>
                         <img alt="pokemon" src={pokemon.image_url} />
                         <p id="number">#{pokemon.number}</p>
                         <p className="nome">{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
